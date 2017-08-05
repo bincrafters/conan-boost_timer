cb3c1554d57536b34f00e84a2009fafd763f8016
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-05 12:39:35 -0400

    add build_requires and settings to conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,13 @@ from conans import ConanFile, tools, os
 class BoostTimerConan(ConanFile):
     name = "Boost.Timer"
     version = "1.64.0"
-    generators = "txt"
+    generators = "txt" 
+    settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/timer"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "timer"
+    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
     requires =  "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.System/1.64.0@bincrafters/testing", \
                       "Boost.Core/1.64.0@bincrafters/testing"
