5b06d77e80433e61aedffbc76b309e927459da3e
Jerry W <jerrywiltse@gmail.com>
2017-09-18 11:51:53 -0400

    change remaining refs to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -12,12 +12,12 @@ class BoostTimerConan(ConanFile):
     lib_short_names = ["timer"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    build_requires = "Boost.Generator/1.64.0@bincrafters/testing" 
-    requires =  "Boost.Chrono/1.64.0@bincrafters/testing", \
-                    "Boost.Config/1.64.0@bincrafters/testing", \
-                    "Boost.Core/1.64.0@bincrafters/testing", \
-                    "Boost.Io/1.64.0@bincrafters/testing", \
-                    "Boost.System/1.64.0@bincrafters/testing"
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing" 
+    requires =  "Boost.Chrono/1.65.1@bincrafters/testing", \
+                    "Boost.Config/1.65.1@bincrafters/testing", \
+                    "Boost.Core/1.65.1@bincrafters/testing", \
+                    "Boost.Io/1.65.1@bincrafters/testing", \
+                    "Boost.System/1.65.1@bincrafters/testing"
  
     def source(self):
         boostorg_github = "https://github.com/boostorg"
