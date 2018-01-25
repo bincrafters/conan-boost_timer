4543ec7b053cb00394e3cc8d4c41a8ca4f8e49e8
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:31 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostTimerConan(ConanFile):
     name = "boost_timer"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_timer"
- 
+     author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["timer"]
     is_header_only = False
 
